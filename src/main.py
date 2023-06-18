9c9e32bd181e3c9a99b9f3014268b3ef39137275
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-18 17:04:14 +0900

    chore(deps): torch

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
+import torch
+
 print("hello world")
+print(torch.__version__)
+print(torch.cuda.is_available())
+print(torch.version.cuda)
+print(torch.backends.cudnn.version())
+print(torch.cuda.get_device_name())
