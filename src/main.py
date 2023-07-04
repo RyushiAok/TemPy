ca3141aca68a179eac2f9caff2d6aa61ea48e1d2
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-07-04 22:39:43 +0900

    feat: add gym env

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import gym
 import torch
 
 print("hello world")
@@ -6,3 +7,17 @@ print(torch.cuda.is_available())
 print(torch.version.cuda)
 print(torch.backends.cudnn.version())
 print(torch.cuda.get_device_name())
+
+env = gym.make("LunarLander-v2", render_mode="human")  # make your environment!
+
+env.action_space.seed(42)
+
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
