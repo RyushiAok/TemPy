d38e8d047257573415638bcbd576dbfc198b303f
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-07-11 11:31:10 +0900

    feat: add python-dotenv

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
-import gym
+from os.path import dirname, join
+
 import torch
+from dotenv import dotenv_values
+
+config = {
+    **dotenv_values(join(dirname(__file__), "../.env.example")),
+    **dotenv_values(join(dirname(__file__), "../.env.local")),
+}
+
+print(config)
 
 print("hello world")
 print(torch.__version__)
@@ -7,17 +16,3 @@ print(torch.cuda.is_available())
 print(torch.version.cuda)
 print(torch.backends.cudnn.version())
 print(torch.cuda.get_device_name())
-
-env = gym.make("LunarLander-v2", render_mode="human")  # make your environment!
-
-env.action_space.seed(42)
-
-observation, info = env.reset(seed=42)
-
-for _ in range(1000):
-    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
-
-    if terminated or truncated:
-        observation, info = env.reset()
-
-env.close()
