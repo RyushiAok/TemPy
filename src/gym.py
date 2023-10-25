8438415acf4f238d8a8bbdf8d8c0474368936fc7
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-10-25 13:00:43 +0900

    chore: apply ruff

--- src/gym.py
+++ src/gym.py
@@ -6,7 +6,9 @@ env.action_space.seed(42)
 observation, info = env.reset(seed=42)
 
 for _ in range(1000):
-    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+    observation, reward, terminated, truncated, info = env.step(
+        env.action_space.sample()
+    )
 
     if terminated or truncated:
         observation, info = env.reset()
